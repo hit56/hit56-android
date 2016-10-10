7b14be8d79e3f1b33afbf613dee37fe6a70a1d19
Stone <reddstones@163.com>
2016-10-10 23:22:53 +0800

    about some REleaseActiviyt.java

---
+++ app/src/main/java/com/hit56/android/activity/ReleaseActivity.java
@@ -0,0 +1,35 @@
+package com.hit56.android.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.hit56.android.R;
+
+public class ReleaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_release);
+        initView();
+    }
+
+    private void initView(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("发布");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
