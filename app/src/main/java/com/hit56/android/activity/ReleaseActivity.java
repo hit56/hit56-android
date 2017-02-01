a89135f6f2e71e8fac79b8ccba90cf3c40dd923d
Stone <reddstones@163.com>
2017-02-01 10:44:54 +0800

    releaseActivity

--- app/src/main/java/com/hit56/android/activity/ReleaseActivity.java
+++ app/src/main/java/com/hit56/android/activity/ReleaseActivity.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.hit56.android.R;
 
-public class ReleaseActivity extends AppCompatActivity {
+public class ReleaseActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText mEditText;
+    private ImageView mImageView;
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +29,12 @@ public class ReleaseActivity extends AppCompatActivity {
         toolbar.setTitle("发布");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mButton = (Button) findViewById(R.id.release_button);
+        mEditText = (EditText) findViewById(R.id.release_text_input);
+        mImageView = (ImageView) findViewById(R.id.release_add_image);
+
+        mButton.setOnClickListener(this);
+        mImageView.setOnClickListener(this);
     }
 
 
@@ -31,5 +45,23 @@ public class ReleaseActivity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 点击事件处理
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.release_button://确认发布
+                if(mEditText.getText().toString().isEmpty() || mEditText.getText().toString().trim().isEmpty()){
+                    return;
+                }
+                break;
+            case R.id.release_add_image://添加图片
+                break;
+            default:
+                break;
+        }
     }
 }
